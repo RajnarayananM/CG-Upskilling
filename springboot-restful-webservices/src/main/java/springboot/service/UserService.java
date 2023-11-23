197697568fa02443b5f25e8e8757d6893a4744bb
RajnarayananM <99036674+RajnarayananM@users.noreply.github.com>
2023-11-23 15:13:45 +0530

    webservices final

---
+++ springboot-restful-webservices/src/main/java/springboot/service/UserService.java
@@ -0,0 +1,17 @@
+package springboot.service;
+
+import java.util.List;
+
+import springboot.entity.User;
+
+public interface UserService {
+    User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+}
