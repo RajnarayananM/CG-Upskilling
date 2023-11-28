82d52697d270952b6a167a54181de1bfa5446564
RajnarayananM <99036674+RajnarayananM@users.noreply.github.com>
2023-11-28 21:07:53 +0530

    Kafka final

---
+++ springboot-kafka-microservices/email-service/src/test/java/demo/raj/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.raj.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
