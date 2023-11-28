7de36ed42a390d1b0e65c8f3d428c616745c0955
RajnarayananM <99036674+RajnarayananM@users.noreply.github.com>
2023-11-28 21:16:26 +0530

    Kafka

---
+++ springboot-kafka-tutorial/src/main/java/demo/raj/springboot/controller/JsonMessageController.java
@@ -0,0 +1,27 @@
+package demo.raj.springboot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.raj.springboot.kafka.JsonKafkaProducer;
+import demo.raj.springboot.payload.User;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+}
